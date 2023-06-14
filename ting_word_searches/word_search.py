c1deacfb554cdbcbdd5fd209f3ae874186ae905b
Thiago Omoto <thi.omoto@gmail.com>
2023-06-14 18:09:35 -0300

    implementando a função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for index in range(len(instance)):
+        path = instance.search(index)
+        file = {
+            "palavra": word,
+            "arquivo": path["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for p in path["linhas_do_arquivo"]:
+            if word.lower() in p.lower():
+                file["ocorrencias"].append(
+                    {"linha": path["linhas_do_arquivo"].index(p) + 1}
+                )
+        if file["ocorrencias"]:
+            result.append(file)
+    return result
 
 
 def search_by_word(word, instance):
